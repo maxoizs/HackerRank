7338b012816de7f614ed826699597cbf1ac7506e
Mohammad Mahmoud <Mohammad.Mahmoud@btl.com>
2018-03-19 14:02:40 +0000

    first commit

---
+++ HackerRank/CamelCase.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NUnit;
+
+namespace HackerRank
+{
+    [TestFixture]
+    public class CamelCase
+    {
+        private int Chanllange(string s)
+        {
+            var counter = s.Length > 0 ? 1 : 0; 
+            foreach (var c in s)
+            {
+                if (char.IsUpper(c))
+                {
+                    counter++; 
+                }
+            }
+            return counter;
+        }
+
+        [TestCase("a", 1)]
+        [TestCase("as", 1)]
+        [TestCase("inThe", 2)]
+        [TestCase("inTheWorld", 3)]
+        [Test]
+        public void Test(string word, int count)
+        {
+            var result = Chanllange(word);
+            Assert.That(result, Is.EqualTo(count));
+        }
+    }
+}
---
+++ HackerRank/SuperReducedString.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using NUnit;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HackerRank
+{
+    [TestFixture]
+    public class SuperReducedString
+    {
+        private string Chanllange(string s)
+        {
+            var result = new List<char>();
+
+            for(var x =0; x<s.Length; x++)
+            {                
+                if(x < s.Length - 1 && s[x]==s[x+1])                
+                {
+                    x += 1; 
+                }
+                else
+                {                   
+                    if(result.Any() && result.Last() == s[x])
+                    {
+                        result.RemoveAt(result.Count - 1);
+                    }
+                    else
+                    {
+                        result.Add(s[x]);
+                    }
+                }
+            }
+            var output = string.Join(string.Empty, result);
+
+            return output==string.Empty? "Empty String": output; 
+        }
+
+
+        [TestCase("dcabaabacd", "Empty String")]
+        [TestCase("baab", "Empty String")]
+        [TestCase("aa","Empty String")]
+        [TestCase("i", "i")]
+        [TestCase("iiiiiii", "i")]
+        [TestCase("aaabccddd", "abd")]
+        [TestCase("inTheWorld", "inTheWorld")]
+        [Test]
+        public void Test(string word, string expected)
+        {
+            var result = Chanllange(word);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
